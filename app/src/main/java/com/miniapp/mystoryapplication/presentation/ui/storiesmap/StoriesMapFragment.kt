cc9e386df3ea9982aa4cf6e8ff33ef5c132c3896
FREDYKA MAULANA <fredyka.maulana@gmail.com>
2023-01-19 10:59:12 +0700

    add map marker methode

--- app/src/main/java/com/miniapp/mystoryapplication/presentation/ui/storiesmap/StoriesMapFragment.kt
+++ app/src/main/java/com/miniapp/mystoryapplication/presentation/ui/storiesmap/StoriesMapFragment.kt
@@ -1,13 +1,22 @@
 package com.miniapp.mystoryapplication.presentation.ui.storiesmap
 
 import android.os.Bundle
+import androidx.navigation.fragment.navArgs
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
+import com.miniapp.mystoryapplication.presentation.responseui.UserLocationData
 
 class StoriesMapFragment : SupportMapFragment(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+
+    private val boundsBuilder = LatLngBounds.builder()
+    private val navArg: StoriesMapFragmentArgs by navArgs()
+    private val userLocationData by lazy { navArg.userLocationData }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +25,29 @@ class StoriesMapFragment : SupportMapFragment(), OnMapReadyCallback {
 
     override fun onMapReady(map: GoogleMap) {
         mMap = map
+        addMapMarkers(userLocationData)
+        addMapBoundView()
+    }
+
+    private fun addMapMarkers(userLocationData: UserLocationData) {
+        userLocationData.userLatLng.forEachIndexed { i, latLng ->
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .title(userLocationData.userInfo[i])
+            )?.showInfoWindow()
+            boundsBuilder.include(latLng)
+        }
+    }
+
+    private fun addMapBoundView() {
+        mMap.animateCamera(
+            CameraUpdateFactory.newLatLngBounds(
+                boundsBuilder.build(),
+                resources.displayMetrics.widthPixels,
+                resources.displayMetrics.heightPixels,
+                300
+            )
+        )
     }
 }
