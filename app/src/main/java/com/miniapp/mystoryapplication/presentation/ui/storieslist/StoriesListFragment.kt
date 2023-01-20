bf1245d7eca5ad95cddcaf7c083cc93edbdf15eb
FREDYKA MAULANA <fredyka.maulana@gmail.com>
2023-01-20 17:42:01 +0700

    change get stories to paging method

--- app/src/main/java/com/miniapp/mystoryapplication/presentation/ui/storieslist/StoriesListFragment.kt
+++ app/src/main/java/com/miniapp/mystoryapplication/presentation/ui/storieslist/StoriesListFragment.kt
@@ -6,31 +6,30 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.material.snackbar.Snackbar
+import androidx.paging.LoadState
+import androidx.paging.map
 import com.miniapp.mystoryapplication.R
 import com.miniapp.mystoryapplication.core.abstraction.BaseFragment
 import com.miniapp.mystoryapplication.core.abstraction.OnItemClickListener
 import com.miniapp.mystoryapplication.core.delegates.viewBinding
-import com.miniapp.mystoryapplication.data.utils.ResultState
 import com.miniapp.mystoryapplication.databinding.FragmentStoriesListBinding
-import com.miniapp.mystoryapplication.domain.mapper.mapStoriesDomainToUi
-import com.miniapp.mystoryapplication.domain.responsedomain.StoriesResponseDomainModel
 import com.miniapp.mystoryapplication.presentation.responseui.StoriesResponseUiModel
-import com.miniapp.mystoryapplication.presentation.responseui.UserLocationData
-import com.miniapp.mystoryapplication.presentation.ui.storieslist.adapter.StoryListAdapter
+import com.miniapp.mystoryapplication.presentation.ui.storieslist.adapter.LoadingStateAdapter
+import com.miniapp.mystoryapplication.presentation.ui.storieslist.adapter.StoryListPagingAdapter
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class StoriesListFragment : BaseFragment(R.layout.fragment_stories_list), OnItemClickListener {
     private val binding by viewBinding(FragmentStoriesListBinding::bind)
     private val vm: StoriesListViewModel by viewModel()
+    private val pagingAdapter: StoryListPagingAdapter by lazy { StoryListPagingAdapter(this) }
 
-    private var adapter: StoryListAdapter? = null
     private var isSavedInstanceState = false
-
-    private lateinit var userLocationData: UserLocationData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,72 +38,54 @@ class StoriesListFragment : BaseFragment(R.layout.fragment_stories_list), OnItem
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        adapter = StoryListAdapter(this)
         isSavedInstanceState = savedInstanceState === null
 
-        setupObservers()
         setupView()
-        setHasOptionsMenu(true)
     }
 
     override fun onResume() {
         super.onResume()
-        if (isSavedInstanceState) vm.getStoriesList()
+        if (isSavedInstanceState) getStories()
     }
 
-    private fun setupObservers() {
-        vm.getStoriesList.observe(viewLifecycleOwner) { data ->
-            when (data) {
-                is ResultState.Loading -> {
-                    binding.containerLoadingProgress.root.visibility = View.VISIBLE
-                }
-                is ResultState.Success -> {
-                    binding.containerLoadingProgress.root.visibility = View.GONE
-                    mapUserLocationData(data.data)
-
-                    val storiesList = data.data?.listStory?.map {
-                        mapStoriesDomainToUi.map(it)
-                    }
-                    if (storiesList?.isNotEmpty() == true) {
-                        adapter?.submitList(storiesList)
-                        binding.rvStoriesList.visibility = View.VISIBLE
-                    } else {
-                        binding.rvStoriesList.visibility = View.GONE
-                        binding.containerEmptyData.root.visibility = View.VISIBLE
-                    }
-                }
-                is ResultState.Error -> {
-                    binding.containerLoadingProgress.root.visibility = View.GONE
-                    showSnackBar(
-                        binding.root, data.message ?: "", duration = Snackbar.LENGTH_LONG
-                    ) { vm.getStoriesList() }
-                }
+    private fun getStories() {
+        lifecycleScope.launch {
+            pagingAdapter.loadStateFlow.collectLatest { loadStates ->
+                binding.containerLoadingProgress.root.isVisible =
+                    loadStates.refresh is LoadState.Loading
+            }
+        }
+        lifecycleScope.launch {
+            vm.getStoriesList(pageSize = PAGE_SIZE).collectLatest {
+                pagingAdapter.submitData(it.map { story ->
+                    StoriesResponseUiModel(
+                        id = story.id,
+                        name = story.name,
+                        description = story.description,
+                        photoUrl = story.photoUrl,
+                        createdAt = story.createdAt,
+                        lat = story.lat,
+                        lon = story.lon
+                    )
+                })
             }
         }
     }
 
-    private fun mapUserLocationData(storiesList: StoriesResponseDomainModel?) {
-        val userInfo = mutableListOf<String>()
-        val userLatLng = mutableListOf<LatLng>()
-
-        storiesList?.listStory?.forEach {
-            userInfo.add("${it?.name.orEmpty()}: ${it?.description.orEmpty()}")
-            userLatLng.add(
-                LatLng(
-                    it?.lat?.toDouble() ?: 0.0,
-                    it?.lon?.toDouble() ?: 0.0
-                )
-            )
-        }
-
-        userLocationData = UserLocationData(
-            userInfo = userInfo,
-            userLatLng = userLatLng
-        )
+    private fun setupView() {
+        binding.rvStoriesList.adapter =
+            pagingAdapter.apply {
+                withLoadStateFooter(footer = LoadingStateAdapter { pagingAdapter.retry() })
+                addLoadStateListener { handleEmptyState() }
+            }
     }
 
-    private fun setupView() {
-        binding.rvStoriesList.adapter = adapter
+    private fun handleEmptyState() {
+        if (pagingAdapter.itemCount == 0) {
+            binding.containerEmptyData.root.visibility = View.VISIBLE
+        } else {
+            binding.containerEmptyData.root.visibility = View.GONE
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -116,7 +97,7 @@ class StoriesListFragment : BaseFragment(R.layout.fragment_stories_list), OnItem
         when (item.itemId) {
             R.id.mapView -> {
                 findNavController().navigate(
-                    StoriesListFragmentDirections.toStoriesMapFragment(userLocationData)
+                    StoriesListFragmentDirections.toStoriesMapFragment()
                 )
             }
         }
@@ -127,13 +108,11 @@ class StoriesListFragment : BaseFragment(R.layout.fragment_stories_list), OnItem
         val extras = FragmentNavigatorExtras(view to "story_image")
         findNavController().navigate(
             StoriesListFragmentDirections.actionStoriesListFragmentToStoryDetailFragment()
-                .setItem(item as StoriesResponseUiModel),
-            extras
+                .setItem(item as StoriesResponseUiModel), extras
         )
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        adapter = null
+    companion object {
+        private const val PAGE_SIZE = 5
     }
 }
