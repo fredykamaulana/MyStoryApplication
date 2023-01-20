1e8276819144051730dfeeb49134ea3a8d2bb178
FREDYKA MAULANA <fredyka.maulana@gmail.com>
2023-01-20 17:39:46 +0700

    add get stories paging function

--- app/src/main/java/com/miniapp/mystoryapplication/domain/usecase/getstories/GetStoriesUseCases.kt
+++ app/src/main/java/com/miniapp/mystoryapplication/domain/usecase/getstories/GetStoriesUseCases.kt
@@ -1,9 +1,12 @@
 package com.miniapp.mystoryapplication.domain.usecase.getstories
 
+import androidx.paging.PagingData
 import com.miniapp.mystoryapplication.data.utils.ResultState
 import com.miniapp.mystoryapplication.domain.responsedomain.StoriesResponseDomainModel
 import kotlinx.coroutines.flow.Flow
 
 interface GetStoriesUseCases {
     suspend fun getStories(): Flow<ResultState<StoriesResponseDomainModel>>
+
+    fun getStoriesWithPaging(pageSize: Int = 0): Flow<PagingData<StoriesResponseDomainModel.ListStoryItem>>
 }
